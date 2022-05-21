3142c66978db8e4b8a6f5fe9f84ee81aae6993cb
Anant Gabhane <gabhaneanant90@gmail.com>
2022-05-21 15:27:30 +0530

    prints size of array and pointer

---
+++ ques7.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a[5];
+    int *c;
+
+    cout << sizeof(a) << " " << sizeof(c) << endl;
+    //      20                      8
+    return 0;
+}
